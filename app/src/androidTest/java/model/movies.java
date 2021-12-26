2159c4773e81047c5c8124b7ddd700082dbf834c
USER <kawsar.ahmed.cse@ulab.edu.bd>
2021-12-26 19:39:28 +0600

    HELLO WORLD

---
+++ app/src/androidTest/java/model/movies.java
@@ -0,0 +1,16 @@
+package model;
+
+public class movies {
+    Integer id;
+    String moviename;
+    String movieurl;
+    String fillurl;
+
+    public movies(Integer id, String moviename, String movieurl, String fillurl) {
+        this.id = id;
+        this.moviename = moviename;
+        this.movieurl = movieurl;
+        this.fillurl = fillurl;
+    }
+}
+}
